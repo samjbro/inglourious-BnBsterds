8c3d68ffe27b075feab4f384915c05de7033bb41
samjbro <sjbroughton1@gmail.com>
2016-08-09 11:12:22 +0100

    adds name attr to space

--- app/app.rb
+++ app/app.rb
@@ -14,7 +14,13 @@ class InglouriousBnB < Sinatra::Base
   end
 
   post '/spaces' do
-    space = Space.create
+    space = Space.create(name: params[:space_name])
     # space.save
+    redirect('/spaces/all')
+  end
+
+  get '/spaces/all' do
+    @spaces = Space.all
+    erb :'spaces/all'
   end
 end
--- spec/features/creating_a_space_spec.rb
+++ spec/features/creating_a_space_spec.rb
@@ -2,8 +2,8 @@ feature 'Creating a space' do
   scenario "I can create a new space" do
     visit 'spaces/new'
     expect(page).to have_content("List a space")
-    fill_in("name", :with => "Inglorious Apparment")
+    fill_in("space_name", :with => "Inglorious Apartment")
     expect{ click_button('submit') }.to change{ Space.count }.by(1)
-    expect(page).to have_content("Inglorious Appartment")
+    expect(page).to have_content("Inglorious Apartment")
   end
 end
