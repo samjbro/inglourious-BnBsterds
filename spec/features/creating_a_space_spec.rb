bbe59a3445741e89379c4409d281476ad5027783
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-08-09 10:55:54 +0100

    add feature test for adding name to space

--- spec/features/creating_a_space_spec.rb
+++ spec/features/creating_a_space_spec.rb
@@ -2,6 +2,8 @@ feature 'Creating a space' do
   scenario "I can create a new space" do
     visit 'spaces/new'
     expect(page).to have_content("List a space")
+    fill_in("name", :with => "Inglorious Apparment")
     expect{ click_button('submit') }.to change{ Space.count }.by(1)
+    expect(page).to have_content("Inglorious Appartment")
   end
 end
