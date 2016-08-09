3ceb367f0e7147f0e66379251cf62e7828a00744
Alex Saunders <acsauk@gmail.com>
2016-08-09 12:44:30 +0100

    closes #19 password encryption (#24)
    * updates sign up test to check for user added to database
    
    * adds functionality to encrypt user password
    
    * adds functionality to require unique email address and required fields for email and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
+require 'bcrypt'
 
 class User
   include DataMapper::Resource
 
+  attr_reader   :password
+  attr_accessor :password_confirmation
+
   property :id, Serial
-  property :name, String
-  property :email, String
-  # property :password_digest, String, length: 60
+  property :name, String, required: true
+  property :email, String, unique: true, required: true
+  property :password_digest, String, length: 60
+
+  validates_confirmation_of :password
+
+  def password=(password)
+    @password = BCrypt::Password.create(password)
+    self.password_digest = @password
+  end
 
 end
