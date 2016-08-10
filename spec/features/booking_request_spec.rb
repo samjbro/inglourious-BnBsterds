3c0c1f152ce612d30a23bf68bc53e720b08398c3
acsauk <acsauk@gmail.com>
2016-08-10 14:18:48 +0100

    writes failing test for creating booking request

---
+++ spec/features/booking_request_spec.rb
@@ -0,0 +1,15 @@
+feature 'booking requests' do
+
+  scenario 'create booking request' do
+    sign_up
+    create_space
+    visit '/spaces/all'
+    click_button('Space details')
+    click_button('Make booking request')
+    fill_in('start date', :with => '2016/08/09')
+    fill_in('end date', :with => '2016/08/16')
+    click_button('Send booking request')
+    expect(page).to have_content('Booking request made for Inglorious Apartment for dates 2016/08/09 - 2016/08/16')
+  end
+
+end
