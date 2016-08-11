fc681cf946711216611150cdff4be10ce7c6c1e6
acsauk <acsauk@gmail.com>
2016-08-11 12:03:28 +0100

    implements validation in BookingRequest to only save booking requests if requested dates are within space available date range

--- spec/features/booking_request_spec.rb
+++ spec/features/booking_request_spec.rb
@@ -11,7 +11,17 @@ feature 'booking requests' do
     fill_in('end_date', :with => '2016/08/16')
     click_button('Make booking request')
     expect(page).to have_content('Booking request made for Inglorious Apartment for dates 2016-08-09 - 2016-08-16')
-
   end
+
+  scenario 'does not create booking request for unavailable dates' do
+      sign_up
+      create_space
+      visit '/spaces/all'
+      click_button('Space details')
+      fill_in('start_date', :with => '2015/08/09')
+      fill_in('end_date', :with => '2015/08/16')
+      click_button('Make booking request')
+      expect(page).to have_content('The space is unavailable for the dates you have selected')
+    end
 
 end
