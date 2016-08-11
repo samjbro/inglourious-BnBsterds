87226e93246987f52a366917927c442785009447
Robert Summers <rsummers02@gmail.com>
2016-08-11 14:41:07 +0100

    set up tests for landing page search form

--- spec/features/viewing_spaces_spec.rb
+++ spec/features/viewing_spaces_spec.rb
@@ -40,4 +40,19 @@ feature  'Viewing spaces' do
     expect(page).to have_content('early date')
     expect(page).not_to have_content('later date')
   end
+
+  scenario 'I can some available spaces before I sign-in' do
+    create_space(name: 'early date',
+                start_date: '2016/08/09',
+                end_date: '2016/08/16')
+    create_space(name: 'later date',
+                start_date: '2016/09/01',
+                end_date: '2016/09/16')
+    visit('/')
+    fill_in(:available_from, with: '2016/08/10')
+    fill_in(:available_to, with: '2016/09/16')
+    click_button("Search")
+    expect(current_path).to equal('/spaces/all')
+    expect(page).to have_content('early date')
+  end
 end
