2024b8422940436997333dec19bb2def8bbe7038
samjbro <sjbroughton1@gmail.com>
2016-08-12 12:54:54 +0100

    prevents double booking of spaces

--- app/models/booking_request.rb
+++ app/models/booking_request.rb
@@ -17,9 +17,13 @@ class BookingRequest
                         :when => [:first_submit]
 
   def validates_booking_dates
-    space_range = (@space.start_date.to_date..@space.end_date.to_date)
-    request_range = (@start_date.to_date..@end_date.to_date)
-    true if ( request_range.begin >= space_range.begin && request_range.max <= space_range.max)
+    space_dates = (@space.start_date.to_date..@space.end_date.to_date).to_a
+    other_booking_requests = BookingRequest.all(space: @space, approved: true)
+    other_booking_requests.each do |request|
+      space_dates -= (request.start_date.to_date..request.end_date.to_date).to_a
+    end
+    request_dates = (@start_date.to_date..@end_date.to_date).to_a
+    true if ((space_dates & request_dates) == request_dates)
   end
 
 end
